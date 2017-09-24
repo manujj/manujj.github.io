53dae2ed55ee2a32039f7c21a78c6f7d0a6a5a34
manujj <manujj@users.noreply.github.com>
2017-09-24 19:11:38 +0200

    .

--- layout/scripts/scripts_div.js
+++ layout/scripts/scripts_div.js
@@ -26,7 +26,7 @@ $(document).ready(function(){
 			event.preventDefault();
 
 			var hash = this.hash;
-	  
+
 			// 800 specifies the number of milliseconds it takes to scroll to the specified area
 			$('html, body').animate({
 				scrollTop: $(hash).offset().top
@@ -34,7 +34,7 @@ $(document).ready(function(){
 
 			window.location.hash = hash;
 			});
-		}	
+		}
 	}
   });
 });
@@ -50,23 +50,23 @@ var x = setInterval(function() {
 
     // Get todays date and time
     var now = new Date().getTime();
-    
+
     // Find the distance between now an the count down date
     var distance = countDownDate - now;
-    
+
     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-    
+
     // Output the result in an element with id="counterxx"
-    document.getElementById("counterDays").innerHTML = days;	
+    document.getElementById("counterDays").innerHTML = days;
 	document.getElementById("counterHours").innerHTML = hours;
     document.getElementById("counterMinutes").innerHTML = minutes;
 	document.getElementById("counterSeconds").innerHTML = seconds;
-    
-    // If the count down is over, write some text 
+
+    // If the count down is over, write some text
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("demo").innerHTML = "EXPIRED";
@@ -100,3 +100,33 @@ function onClick(element) {
   var captionText = document.getElementById("caption");
   captionText.innerHTML = element.alt;
 }
+
+/*################ Hover on mobile devices ##################
+########################################################
+########################################################*/
+;(function(){
+	var isTouch = false //var to indicate current input type (is touch versus no touch)
+	var isTouchTimer
+	var curRootClass = '' //var indicating current document root class ("can-touch" or "")
+
+	function addtouchclass(e){
+		clearTimeout(isTouchTimer)
+		isTouch = true
+		if (curRootClass != 'can-touch'){ //add "can-touch' class if it's not already present
+			curRootClass = 'can-touch'
+			document.documentElement.classList.add(curRootClass)
+		}
+		isTouchTimer = setTimeout(function(){isTouch = false}, 500) //maintain "istouch" state for 500ms so removetouchclass doesn't get fired immediately following a touch event
+	}
+
+	function removetouchclass(e){
+		if (!isTouch && curRootClass == 'can-touch'){ //remove 'can-touch' class if not triggered by a touch event and class is present
+			isTouch = false
+			curRootClass = ''
+			document.documentElement.classList.remove('can-touch')
+		}
+	}
+
+	document.addEventListener('touchstart', addtouchclass, false) //this event only gets called when input type is touch
+	document.addEventListener('mouseover', removetouchclass, false) //this event gets called when input type is everything from touch to mouse/ trackpad
+})();
